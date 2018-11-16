6a107e58e6abc93c70c9448910fcb0d95142efe4
jiaqianjing <jiaqianjing@baidu.com>
2018-11-16 11:42:51 +0800

    add testcase

--- ToTestTestCase.py
+++ ToTestTestCase.py
@@ -3,6 +3,7 @@
 import unittest
 
 import mock
+from mock import MagicMock
 
 import to_test
 
@@ -17,14 +18,10 @@ class ToTestTestCase(unittest.TestCase):
     # 如果 patch 多个外部函数，那么调用遵循自下而上的规则
     @mock.patch('to_test.mul')
     @mock.patch('to_test.add')
-    def test_call_other_funcs(self, mock_add, mock_mul):
-        x = 1
-        y = 2
-        a = 3
-        b = 4
+    def test_call_other_funcs_01(self, mock_add, mock_mul):
+        x, y, a, b = 1, 2, 3, 4
         mock_add.return_value = 3
         mock_mul.return_value = 12
-        to_test.global_variable = "KKK"
         result = to_test.call_other_funcs(x, y, a, b)
         # mock_add 确保调用过一次，如果没调用或多于一次，则抛出 AssertionError 异常
         mock_add.assert_called_once()
@@ -32,6 +29,24 @@ class ToTestTestCase(unittest.TestCase):
         print result
         assert result is not None
         assert result == (3, 12)
+
+    """
+    simulate IOError
+    side_effect function:
+        1. set Exception
+        2. set multi value for mock_func or mock_variant which multi called 
+    """
+
+    @mock.patch.object(to_test, 'mul', MagicMock(side_effect=IOError("io error!")))
+    def test_call_other_funcs_02(self):
+        result = to_test.call_other_funcs(1, 2, 3, 4)
+        self.assertIsNone(result)
+
+    # mock variant
+    @mock.patch.object(to_test, 'global_variable', MagicMock(return_value='mock'))
+    def test_call_orther_funcs_03(self):
+        to_test.call_other_funcs(1, 2, 3, 4)
+
 
 
 if __name__ == "__main__":
--- to_test.py
+++ to_test.py
@@ -16,22 +16,25 @@ def mul(a, b):
 
 
 def call_other_funcs(x, y, a, b):
-    demo = "demo"
-    ret01 = add(x, y)
-    ret02 = mul(a, b)
-    print "global_variable: {}".format(global_variable)
-    print "demo: {} ret01: {} ret02: {}".format(demo, ret01, ret02)
+    try:
+        demo = "demo"
+        ret01 = add(x, y)
+        ret02 = mul(a, b)
+        print "global_variable: {}".format(global_variable)
+        print "demo: {} ret01: {} ret02: {}".format(demo, ret01, ret02)
 
-    # 对得到的 ret01, ret02 进行一些处理，得到最终返回值 ret。例如
-    if ret01 and ret02:
-        ret = (ret01, ret02)
-    elif ret01 and (not ret02):
-        ret = (ret01, 0)
-    elif (not ret01) and ret02:
-        ret = (0, ret02)
-    else:
-        ret = (0, 0)
-    return ret
+        # 对得到的 ret01, ret02 进行一些处理，得到最终返回值 ret。例如
+        if ret01 and ret02:
+            ret = (ret01, ret02)
+        elif ret01 and (not ret02):
+            ret = (ret01, 0)
+        elif (not ret01) and ret02:
+            ret = (0, ret02)
+        else:
+            ret = (0, 0)
+        return ret
+    except Exception as e:
+        print "error: {}".format(e)
 
 
 if __name__ == "__main__":
